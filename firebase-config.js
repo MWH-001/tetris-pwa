a85980bf1ea6abe0274d396efa53cccff96c7897
MWH-001 <MWH-001@users.noreply.github.com>
2026-08-19 13:18:03 +0000

    Add Firebase auth and leaderboard

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDkIlCUkss8z3_maUjfHD2JIt7bgLXlnzE",
+  authDomain: "asdf-fe254.firebaseapp.com",
+  projectId: "asdf-fe254",
+  storageBucket: "asdf-fe254.firebasestorage.app",
+  messagingSenderId: "360785203760",
+  appId: "1:360785203760:web:651e40fd05aeb7714c53f5",
+  measurementId: "G-DB9E73N241"
+};
