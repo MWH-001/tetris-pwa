362f7dcad7bbefc929ade4a9b6dcbf9be8fa2d72
MWH-001 <MWH-001@users.noreply.github.com>
2026-08-19 11:09:12 +0000

    Initial: Tetris PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'tetris-v1';
+var FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
